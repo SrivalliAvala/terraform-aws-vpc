566828808813059b6f2488754ec35954ea5e94d2
SrivalliAvala <srivalliavala999@gmail.com>
2024-10-03 14:20:47 +0530

    terraform

--- datasource.tf
+++ datasource.tf
@@ -1,3 +1,15 @@
 data "aws_availability_zones" "available" {   #quering using datasources
   state = "available"   #availability zone is already provided in provider.tf as us-east-1....so it gives that data
 }                       #use output.tf to check
+
+data "aws_vpc" "default" {
+  default = true
+}
+
+data "aws_route_table" "main"{
+  vpc_id = data.aws_vpc.default.id
+  filter {
+    name = "association.main"
+    values = ["true"]
+  }
+}
--- variables.tf
+++ variables.tf
@@ -15,11 +15,13 @@ variable "enable_dns_hostnames" {
 # }
 
 variable "project_name" {
-    type = string   
+    type = string  
+    default = "expense"
 }
 
-variable "Environment" {
+variable "environment" {
     type = string
+    default = "dev" 
 }
 
 variable "common_tags" {
@@ -35,11 +37,12 @@ variable "vpc_tags" {
 }
 
 variable "vpc_cidr" {
-
+    default = "10.0.0.0/16"
 }
 
 variable "public_subnet_cidrs" {
     type = list
+    default = ["10.0.1.0/24", "10.0.2.0/24"]
     validation {
         condition = length(var.public_subnet_cidrs) == 2
         error_message = "Please provide 2 valid public subnet CIDR"
@@ -52,6 +55,7 @@ variable "public_subnet_tags" {
 
 variable "private_subnet_cidrs" {
     type = list
+    default = ["10.0.11.0/24", "10.0.12.0/24"]
     validation {
       condition = length(var.private_subnet_cidrs) == 2
       error_message = "please provide two valid private cidrs"
@@ -64,6 +68,7 @@ variable "private_subnet_tags" {
 
 variable "database_subnet_cidrs" {
     type = list
+    default = ["10.0.21.0/24", "10.0.22.0/24"]
     validation {
       condition = length(var.database_subnet_cidrs) == 2
       error_message = "please provide two valid database cidrs"
@@ -82,3 +87,23 @@ variable "nat_tags" {
   default = {}
 }
 
+variable "public_route_table_tags" {
+    default = {}
+}
+
+variable "private_route_table_tags" {
+    default = {}
+}
+
+variable "database_route_table_tags" {
+    default = {}
+}
+
+variable "is_peering_required" {
+    type = bool
+    default = false
+}
+
+variable "vpc_peering_tags" {
+    default = {}
+}
